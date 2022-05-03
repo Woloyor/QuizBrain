be354f3755c8c8086dcf9ce2a95ebcd4533381d6
Woloyor <yuralysyshak@gmail.com>
2022-05-03 17:23:04 +0300

    QuizApp

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,11 @@
-import 'package:flutter/material.dart';
+import 'dart:ffi';
+import 'package:quizzler/quiz_brain.dart';
 
+import 'question.dart';
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+QuizBrain quizBrain = QuizBrain();
 void main() => runApp(Quizzler());
 
 class Quizzler extends StatelessWidget {
@@ -25,6 +31,30 @@ class QuizPage extends StatefulWidget {
 }
 
 class _QuizPageState extends State<QuizPage> {
+  List<Icon> scoreKeeper = [];
+
+  int randomNumber = Random().nextInt(13);
+
+  void SetCheckAnswer(bool userAnswer) {
+    bool checkanswer = quizBrain.getAnswer(randomNumber = Random().nextInt(13));
+    if (userAnswer == checkanswer) {
+      scoreKeeper.add(Icon(
+        Icons.check,
+        color: Colors.green,
+      ));
+    } else {
+      scoreKeeper.add(Icon(
+        Icons.close,
+        color: Colors.red,
+      ));
+    }
+
+    setState(() {
+      // ignore: unnecessary_statements
+      quizBrain.getAnswer(randomNumber = Random().nextInt(13));
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -37,7 +67,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                'This is where the question text will go.',
+                quizBrain.getQuestionText(randomNumber = Random().nextInt(13)),
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -61,6 +91,7 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
+                SetCheckAnswer(false);
                 //The user picked true.
               },
             ),
@@ -79,16 +110,26 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
-                //The user picked false.
+                SetCheckAnswer(true);
               },
             ),
           ),
         ),
+        Row(
+          children: scoreKeeper,
+        )
         //TODO: Add a Row here as your score keeper
       ],
     );
   }
 }
+
+@override
+Widget build(BuildContext context) {
+  // TODO: implement build
+  throw UnimplementedError();
+}
+
 
 /*
 question1: 'You can lead a cow down stairs but not up stairs.', false,
---
+++ lib/question.dart
@@ -0,0 +1,9 @@
+class Question {
+  String questionText;
+  bool qestionAsnwer;
+
+  Question({String q, bool a}) {
+    questionText = q;
+    qestionAsnwer = a;
+  }
+}
